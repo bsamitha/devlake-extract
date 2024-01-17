1952ba3f8b946cd7a496d70f1401193eec65a6c6
Samitha Basnayake <36471900+bsamitha@users.noreply.github.com>
2024-01-17 12:17:58 +0530

    Update devlake_lab_exe.py

--- devlake_lab_exe.py
+++ devlake_lab_exe.py
@@ -1,3 +1,4 @@
+import jmespath
 import json
 import requests
 
@@ -6,7 +7,7 @@ DEVLAKE_BASEURL = 'http://54.236.25.78:4000/api/'
 GITHUB_BASEURL = 'https://api.github.com/'
 GITHUB_REPO = 'devlake-extract'
 GITHUB_REPO_OWNER = 'bsamitha'
-PRIVATE_TOKEN = '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'
+PRIVATE_TOKEN = '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'
 VERIFY_SSL_CERTIFICATE = True
 
 
@@ -39,13 +40,16 @@ def github_post_request(endpoint, data):
 
 
 blueprint = dl_get(f'blueprints/%s' % BLUEPRINT_ID)
-projectName = blueprint['projectName'] if 'projectName' in blueprint else None
+# projectName = blueprint['projectName'] if 'projectName' in blueprint else None
+projectName = jmespath.search(f"plan[].options[].projectMappings[].projectName", blueprint)[0]
+repoId = jmespath.search(f"plan[].options[].projectMappings[].[scopes[?table=='repos'].rowId[] | [0]]", blueprint)[0][0]
+repoId = repoId.rpartition(':')[-1]
 print(f'gitlab project %s found' % projectName)
 if not projectName:
     raise Exception("No project found")
 # create github issue
 issue = github_post_request(f'repos/%s/%s/issues' % (GITHUB_REPO_OWNER, GITHUB_REPO), {
-    'title': f'gl issue: %s' % projectName,
+    'title': f'%s: %s' % (repoId, projectName),
     'body': 'created by devlake lab exercise script'
 })
 print(f'github issue %s created.' % str(issue['id']))
